5e1bb7a4f20aee08caaa85024e8eb4f5ab440dae
Alex-Rui98 <ruialex98@gmail.com>
2024-03-15 11:29:42 +0000

    Create app.py
    Written with window_main in mind

---
+++ app.py
@@ -0,0 +1,10 @@
+import window_main
+
+#função main
+def main():
+    app_window = MainWindow()
+    app_window.MainWindow.mainloop()
+
+#roda função main se o arquivo for executado
+if __name__ == '__main__':
+    main()
