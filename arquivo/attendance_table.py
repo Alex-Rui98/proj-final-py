ec9aacf97d04433c6ba23487da32624ce9dcc801
Alex-Rui98 <ruialex98@gmail.com>
2024-03-25 11:34:48 +0000

    1.1 Forgot script for cursor

---
+++ arquivo/attendance_table.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+#ligar à base de dados
+conn = sqlite3.connect('func.db')
+
+#criar um cursor
+
+cursor = conn.cursor()
+
+#comando SQL para criar tabela
+
+#id integer pk
+#utilizador text not null unique
+#password text not null
+
+create_table = '''
+    CREATE TABLE IF NOT EXISTS attendance(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+    worker INT (5),
+    day VARCHAR (20),
+    description VARCHAR (150),
+    FOREIGN KEY (worker) REFERENCES funcionario (id)
+     );
+'''
+
+
+#executar o comando SQL com o cursor
+
+cursor.execute(create_table)
+
+#guardar com um commit
+conn.commit()
+
+#fechar a ligação
+conn.close()
